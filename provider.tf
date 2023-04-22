c470fbec1426be3cb1c26147c71adc7e094d311c
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-22 19:03:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
  region = "ap-south-1"
- access_key= "AKIATX2SH7JYNO6GQ75V"
- secret_key= "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"
+ access_key= "AKIAQDUXA2NVR2VQLCFR"
+ secret_key= "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"
 }
