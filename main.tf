34e154a2e60cdb9d02458b0749bd836fa2ebc40d
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-22 22:08:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "test-server" {
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("AssignmentKey.pem")
+   
     host     = self.public_ip
   }
   provisioner "remote-exec" {
