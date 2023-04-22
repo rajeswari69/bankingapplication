35b943f89d3f4ba0e5eed5df2b76964ca259e197
root <root@ip-172-31-4-182.ap-south-1.compute.internal>
2023-04-22 09:26:32 +0000

    terraform files added

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-03a933af70fa97ad2" 
+  instance_type = "t2.micro" 
+  key_name = "Assignmentkey"
+  vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./Assignmentkey.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+    
+}
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
- region = "us-east-1"
- access_key= "AKIAYOY47DPG2J3LFR5W"
- secret_key= "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"
+ region = "ap-south-1a"
+ access_key= "AKIATX2SH7JYNO6GQ75V"
+ secret_key= "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"
 }
