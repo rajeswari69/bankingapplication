fbf9822540097425d7eced37076e7a24374518ad
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-22 19:50:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-03a933af70fa97ad2" 
   instance_type = "t2.micro" 
-  key_name = file("./AssignmentKey.pem")
+  key_name = "AssignmentKey"
   vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
-  
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./Assignmentkey.pem")
+    host     = self.public_ip
+  }
   provisioner "remote-exec" {
     inline = [ "echo 'wait to start instance' "]
   }
