39e936683e43dd2803b01415cf766e0f2e93cf3e
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-22 22:11:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-03a933af70fa97ad2" 
   instance_type = "t2.micro" 
-  key_name = "AssignmentKey"
   vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-   
     host     = self.public_ip
   }
   provisioner "remote-exec" {
