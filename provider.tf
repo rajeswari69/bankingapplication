fe495bac6da04e01981dd8ae18a61403dad584ba
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-25 09:30:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
  region = "ap-south-1"
- access_key= "AKIAQDUXA2NVT3HH6BN2"
+ access_key= ${{ secrets.AWS_ACCESS_KEY_ID }}
  secret_key= "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"
 }
