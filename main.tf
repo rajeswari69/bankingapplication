5b1f3b8dc8394fc5793ac0279ee21687279fdf85
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-25 20:22:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "test" {
     Name = "test-server"
   }
   provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+        command = " echo ${aws_instance.test.public_ip} > inventory "
   }
   
     
