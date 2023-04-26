8b06052484dd098bd90412cb3de4f37c5326eb63
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-26 09:09:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "test" {
   ami           = "ami-03a933af70fa97ad2" 
   instance_type = "t2.micro" 
   key_name = "AssignmentKey"
-  vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
-  
+    
   tags = {
     Name = "test-server"
   }
