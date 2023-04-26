c87df31ee4c6005667da30dc1d1f635d625ba65d
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-26 08:58:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,7 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro" 
   key_name = "AssignmentKey"
   vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = "AssignmentKey.pem"
-    host     = self.public_ip
-  }
+  
   
       
 }
