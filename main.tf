ec06ab715135a319dc7aadc67fbffed88eec3a98
Aditya3328 <46055209+Aditya3328@users.noreply.github.com>
2023-04-26 10:39:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
-resource "aws_instance" "test" {
-  ami           = "ami-03a933af70fa97ad2" 
-  instance_type = "t2.micro" 
-  key_name = "AssignmentKey"
-    
-  tags = {
-    Name = "test-server"
+resource "aws_instance" "example" {
+  ami           = "ami-02eb7a4783e7e9317" // set the AMI ID of the image you want to use
+  instance_type = "t2.micro" // set the instance type you want to use
+  key_name      = "AssignmentKey" // set the name of the key pair you want to use to SSH into the instance
+  vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "AssignmentKey.pem"
+    host     = self.public_ip
   }
-      
+  tags = {
+    Name = "demo-instance" // set a name for the instance
+  }
+    provisioner "local-exec" {
+        command = " echo ${aws_instance.example.public_ip} > inventory "
+        
+  }
 }
