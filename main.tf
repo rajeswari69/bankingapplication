882cf6f4b810404023a1a95b34a5766056345adc
root <root@ip-172-31-9-86.ap-south-1.compute.internal>
2023-05-10 15:39:20 +0000

    add banking application

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
+provider "aws"{
+ region = "ap-south-1"
+
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-02eb7a4783e7e9317" // set the AMI ID of the image you want to use
-  instance_type = "t2.micro" // set the instance type you want to use
-  key_name      = "AssignmentKey" // set the name of the key pair you want to use to SSH into the instance
-  vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
+  ami           = "ami-02eb7a4783e7e9317" 
+  instance_type = "t2.micro" 
+  key_name      = "banking" 
+  vpc_security_group_ids= ["sg-08b79d8951441ffa1"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("AssignmentKey.pem")
+    private_key = file("banking.pem")
     host     = self.public_ip
   }
   tags = {
-    Name = "deploy-server" // set a name for the instance
+    Name = "testserver"
   }
     provisioner "local-exec" {
          command = "echo ${aws_instance.example.public_ip} > inventory"
